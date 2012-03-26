1f6734f68c4cf773ac853d377d087855d669f492
Mike King <mike@ikayzo.com>
2012-03-25 22:37:52 -1000

    Refactored code to work as plugin

--- jQuery.menutron.js
+++ jQuery.menutron.js
@@ -1,94 +1,74 @@
 /*
-Author: Mike King (@micjamking)
-*/
+ * Copyright (c) 2012 Mike King @micjamking (http://dev.85pixels.com)
+ * 
+ * jQuery Menutron plugin
+ * Version 1.0 (Aug 2012)
+ *
+ * Licensed under the MIT License
+ */
 
-// Media query for device screens < 600px width
-if (screen.width < 600) {					
-	$(document).ready(function(){
-		
-		// Create an empty select menu		
-		var list = $('<select>');
-		
-		// Loop through the current ul list menu & adds the li's text
-		// & the anchors url to the option & it's value respectively.	
-		$('nav ul li').each(function() { 
-			var option = $('<option>').text($(this).text());
-			var link = $(this).children("a").attr("href");
-			$(option).attr("value", link);
-			$(option).appendTo(list);
-		});
-		
-		// Adds an option item to inform the user to Choose
-		var selectedNav = ['<option selected="selected" value>Choose...</option>'].join("");
-		
-		// Appends the newly created list to main nav element
-		$(list).prepend(selectedNav);
-		list.appendTo($('nav'));		
-		
-		// Change Window.location, ie. the current url, to the value
-		// of the selected option
-		$('select').change(function(){
-			if($(this).val()!=''){
-				window.location.href=$(this).val();
+
+(function($){
+	$.fn.menutron = function(options) {
+
+		var defaults = {
+			screenSize: 600,
+		};
+
+		var options = $.extend(defaults, options);
+
+		return this.each(function() {
+			
+			var menu = $(this);
+			var screenSize = $(options.screenSize);
+
+			init();
+
+			function init() {
+				transformMenu();
 			}
+
+			function transformMenu() {
+				// Media query for device screens < 600px width
+				//var deviceWidth = $("(max-width: " + screenSize + "px)");
+				//console.log(deviceWidth);
+				if (window.matchMedia("(max-width: 600px)").matches) {				
+					$(document).ready(function(){
+
+						// Create an empty select menu		
+						var list = $('<select>');
+
+						// Loop through the current ul list menu & adds the li's text
+						// & the anchors url to the option & it's value respectively.	
+						$(menu).find('li').each(function() { 
+							var option = $('<option>').text($(this).text());
+							var link = $(this).children("a").attr("href");
+							$(option).attr("value", link);
+							$(option).appendTo(list);
+						});
+
+						// Adds an option item to inform the user to Choose
+						var selectedNav = ['<option selected="selected" value>Choose...</option>'].join("");
+
+						// Appends the newly created list to main nav element
+						$(list).prepend(selectedNav);
+						list.appendTo($(menu));
+
+						// Removes the original ul list from the display
+						$('ul').css("display", "none");	
+
+						// Change Window.location, ie. the current url, to the value
+						// of the selected option
+						$('select').change(function(){
+							if($(this).val()!=''){
+								window.location.href=$(this).val();
+							}
+						});
+					});
+				}
+			}
+
 		});
-		
-		// Removes the original ul list from the display
-		var nav = $('nav ul').remove();
-	});
-}
+	};
 
-
-/******************* 
- Plugin Architecture 
-*******************/
-
-
-/*(function($){
-    $.yourPluginName = function(el, radius, options){
-        // To avoid scope issues, use 'base' instead of 'this'
-        // to reference this class from internal events and functions.
-        var base = this;
-
-        // Access to jQuery and DOM versions of element
-        base.$el = $(el);
-        base.el = el;
-
-        // Add a reverse reference to the DOM object
-        base.$el.data("yourPluginName", base);
-
-        base.init = function(){
-            if( typeof( radius ) === "undefined" || radius === null ) radius = "20px";
-
-            base.radius = radius;
-
-            base.options = $.extend({},$.yourPluginName.defaultOptions, options);
-
-            // Put your initialization code here
-        };
-
-        // Sample Function, Uncomment to use
-        // base.functionName = function(paramaters){
-        //
-        // };
-
-        // Run initializer
-        base.init();
-    };
-
-    $.yourPluginName.defaultOptions = {
-        radius: "20px"
-    };
-
-    $.fn.yourPluginName = function(radius, options){
-        return this.each(function(){
-            (new $.yourPluginName(this, radius, options));
-
-		   // HAVE YOUR PLUGIN DO STUFF HERE
-
-		   // END DOING STUFF
-
-        });
-    };
-
-})(jQuery);*/
+})(jQuery);
