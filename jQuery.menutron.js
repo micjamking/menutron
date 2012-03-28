288371e7deea825071bcd27a9798eba579408377
Mike King <mike@ikayzo.com>
2012-03-27 20:35:04 -1000

    Configured options
    - added option for user to control screen width
    - loop is now grabbing the children of the list element instead of using find and searching for an 'li'
    - checks for descriptive lists and skips over dt's

--- jQuery.menutron.js
+++ jQuery.menutron.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2012 Mike King @micjamking (http://dev.85pixels.com)
  * 
  * jQuery Menutron plugin
- * Version 1.0 (Aug 2012)
+ * Version 1.0 (March 2012)
  *
  * Licensed under the MIT License
  */
@@ -12,7 +12,7 @@
 	$.fn.menutron = function(options) {
 
 		var defaults = {
-			screenSize: 600,
+			maxScreenWidth: 600
 		};
 
 		var options = $.extend(defaults, options);
@@ -20,7 +20,7 @@
 		return this.each(function() {
 			
 			var menu = $(this);
-			var screenSize = $(options.screenSize);
+			var screenWidth = "(max-width: " + options.maxScreenWidth + "px)";
 
 			init();
 
@@ -29,36 +29,43 @@
 			}
 
 			function transformMenu() {
-				// Media query for device screens < 600px width
-				//var deviceWidth = $("(max-width: " + screenSize + "px)");
-				//console.log(deviceWidth);
-				if (window.matchMedia("(max-width: 600px)").matches) {				
+				
+				// Media query for device screens (default: 600px)
+				if (window.matchMedia(screenWidth).matches) {				
 					$(document).ready(function(){
 
 						// Create an empty select menu		
-						var list = $('<select>');
+						var selectMenu = $('<select>');
+						
+						// Get the list within the child container
+						var children = $(menu).children();
 
-						// Loop through the current ul list menu & adds the li's text
-						// & the anchors url to the option & it's value respectively.	
-						$(menu).find('li').each(function() { 
-							var option = $('<option>').text($(this).text());
-							var link = $(this).children("a").attr("href");
-							$(option).attr("value", link);
-							$(option).appendTo(list);
+						// Loop through the current list menu & adds the li's text
+						// & the anchors url to the option & it's value respectively.
+						$(children).children().each(function() {
+							
+							// If using a descriptive list, 'dl',
+							// this only adds 'dd' to selectMenu & skips over 'dt'
+							if($(this).get(0).tagName !== 'DT'){
+								var option = $('<option>').text($(this).text());
+								var link = $(this).children("a").attr("href");
+								$(option).attr("value", link);
+								$(option).appendTo(selectMenu);
+							}
 						});
 
 						// Adds an option item to inform the user to Choose
 						var selectedNav = ['<option selected="selected" value>Choose...</option>'].join("");
 
-						// Appends the newly created list to main nav element
-						$(list).prepend(selectedNav);
-						list.appendTo($(menu));
+						// Appends the newly created list to menu's container element
+						$(selectMenu).prepend(selectedNav);
+						selectMenu.appendTo($(menu));
 
-						// Removes the original ul list from the display
-						$('ul').css("display", "none");	
+						// Hides the original menu list from the display
+						$(children).css("display", "none");	
 
-						// Change Window.location, ie. the current url, to the value
-						// of the selected option
+						// Change Window.location, ie. the current url,
+						// to the value of the selected option
 						$('select').change(function(){
 							if($(this).val()!=''){
 								window.location.href=$(this).val();
