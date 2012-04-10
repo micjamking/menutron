3ab27bb1c57b1247c53fef95fe826e5423f55ab0
Mike King <mike@ikayzo.com>
2012-04-09 23:11:02 -1000

    Added event trigger for responsive menu
    - moved selectMenu & $(menu).children global variables
    - split up menu creation and transformation in to separate functions
    - created window resize trigger using .resize event

--- jQuery.menutron.js
+++ jQuery.menutron.js
@@ -18,60 +18,89 @@
 
 		return this.each(function() {
 			
-			var menu = $(this);
-			var screenWidth = "(max-width: " + options.maxScreenWidth + "px)";
+			var menu = $(this).children();
+			var screenWidth = "(max-width: " + options.maxScreenWidth + "px)";		
+			var selectMenu = $('<select>').css("display", "none");	
 
 			init();
 
 			function init() {
+				createMenu();
 				transformMenu();
 			}
 
-			function transformMenu() {
+			function createMenu(){
+
+				// Loop through the current list menu & adds the li's text
+				// & the anchors url to the option & it's value respectively.
+				$(menu).children().each(function() {
 				
+					// If using a descriptive list, 'dl',
+					// this only adds 'dd' to selectMenu & skips over 'dt'
+					if($(this).get(0).tagName !== 'DT'){
+						var option = $('<option>').text($(this).text());
+						var link = $(this).children("a").attr("href");
+						$(option).attr("value", link);
+						$(option).appendTo(selectMenu);
+					} 
+				});
+
+				// Adds an option item to inform the user to Choose
+				// FYI, I would like to replace this with the dl's dt if present
+				//if ($(menu).find('dt')){
+				//	var menuTitle = ['<option selected="selected" value>' + $(menu).find('dt').text() + '</option>'].join("");
+				//	$(selectMenu).prepend(menuTitle);
+				//} else {
+				//	var menuTitle = ['<option selected="selected" value>Choose...</option>'].join("");
+				//	$(selectMenu).prepend(menuTitle);
+				//}
+
+				var menuTitle = ['<option selected="selected" value>Choose...</option>'].join("");
+
+				// Appends the newly created list to menu's container element
+				$(selectMenu).prepend(menuTitle);
+				selectMenu.appendTo($(menu).parent());
+
+				// Change Window.location, ie. the current url,
+				// to the value of the selected option
+				$('select').change(function(){
+					if($(this).val()!=''){
+						window.location.href=$(this).val();
+					}
+				});
+
+			}
+
+			function transformMenu() {
+
 				// Media query for device screens (default: 600px)
-				if (window.matchMedia(screenWidth).matches) {				
-					$(document).ready(function(){
+				if (window.matchMedia(screenWidth).matches) {
 
-						// Create an empty select menu		
-						var selectMenu = $('<select>');
-						
-						// Get the list within the child container
-						var children = $(menu).children();
+					// Hides the original menu list from the display
+					$(selectMenu).css("display", "inline-block");
 
-						// Loop through the current list menu & adds the li's text
-						// & the anchors url to the option & it's value respectively.
-						$(children).children().each(function() {
-							
-							// If using a descriptive list, 'dl',
-							// this only adds 'dd' to selectMenu & skips over 'dt'
-							if($(this).get(0).tagName !== 'DT'){
-								var option = $('<option>').text($(this).text());
-								var link = $(this).children("a").attr("href");
-								$(option).attr("value", link);
-								$(option).appendTo(selectMenu);
-							}
-						});
+					// Hides the original menu list from the display
+					$(menu).css("display", "none");
+				}
 
-						// Adds an option item to inform the user to Choose
-						var selectedNav = ['<option selected="selected" value>Choose...</option>'].join("");
-
-						// Appends the newly created list to menu's container element
-						$(selectMenu).prepend(selectedNav);
-						selectMenu.appendTo($(menu));
+				// Event trigger for responsive menu
+				$(window).resize(function(){
+					if ($(window).width() < options.maxScreenWidth){
 
 						// Hides the original menu list from the display
-						$(children).css("display", "none");	
+						$(selectMenu).css("display", "inline-block");
 
-						// Change Window.location, ie. the current url,
-						// to the value of the selected option
-						$('select').change(function(){
-							if($(this).val()!=''){
-								window.location.href=$(this).val();
-							}
-						});
-					});
-				}
+						// Hides the original menu list from the display
+						$(menu).css("display", "none");				
+					} else {
+
+						// Hides the original menu list from the display
+						$(selectMenu).css("display", "none");
+
+						// Show the original menu list
+						$(menu).css("display", "block");
+					}
+				});
 			}
 
 		});
